90feee870a55bbbb9725dd3d0db056e3fb0a15e8
Hugo Rosenkranz-Costa <hugo.rosenkranz@cosmian.com>
2024-01-11 15:15:37 +0100

    split text at the token level and process every chunks in a single batch

--- docker-image/src/summarizer.py
+++ docker-image/src/summarizer.py
@@ -4,14 +4,13 @@ from transformers.tools import TextSummarizationTool
 class Summarizer(TextSummarizationTool):
     def __init__(self, min_tokens_length=30, max_tokens_length=100, **kwargs):
         super().__init__(**kwargs)
-        self.min_tokens_length = 30
-        self.max_tokens_length = 100
+        self.min_tokens_length = min_tokens_length
+        self.max_tokens_length = max_tokens_length
 
     def encode(self, text):
         """Additional checks on the size of the input"""
-
         preprocess_text = text.strip().replace("\n", "")
-        input_tokens = self.pre_processor(
+        input_tokens = self.pre_processor.encode(
             preprocess_text, return_tensors="pt", truncation=True
         )
         if len(input_tokens[0]) < self.min_tokens_length:
--- docker-image/src/translator.py
+++ docker-image/src/translator.py
@@ -1,16 +1,75 @@
+import torch
 from transformers.tools import TranslationTool
 
 
-def chunks(words, chunk_size):
-    for i in range(0, len(words), chunk_size):
-        yield " ".join(words[i : i + chunk_size])
+def find_separator_index(input_tokens, sep_token, start_index):
+    split_index = start_index
+    while input_tokens[split_index] != sep_token:
+        split_index -= 1
+        if split_index == 0:
+            return start_index
+    return split_index
+
+
+def end_pad_tokens(input_tokens, length, pad_token, eos_token):
+    input_tokens.extend([pad_token] * (length - len(input_tokens) - 1))
+    input_tokens.append(eos_token)
+    assert len(input_tokens) == length
+    return input_tokens
 
 
 class Translator(TranslationTool):
-    def __call__(self, text, *args, chunk_size=400, **kwargs):
-        res = []
-        for chunk in chunks(text.split(" "), chunk_size):
-            print("In chunk")
-            res.append(super().__call__(chunk, *args, **kwargs))
+    def __init__(self, chunk_size=500, **kwargs):
+        super().__init__(**kwargs)
+        self.max_tokens_length = chunk_size
 
-        return " ".join(res)
+    def split_chunks(self, input_tokens, bos_token, eos_token):
+        if len(input_tokens) <= self.max_tokens_length:
+            return torch.tensor([input_tokens])
+
+        sep_token = self.pre_processor.encode(".", add_special_tokens=False)[0]
+
+        chunks = []
+        # Split at the previous `sep` closer to `max_tokens_length`
+        while len(input_tokens) > self.max_tokens_length:
+            split_index = find_separator_index(
+                input_tokens, sep_token, self.max_tokens_length - 1
+            )
+            chunks.append(
+                end_pad_tokens(
+                    input_tokens[:split_index],
+                    self.max_tokens_length,
+                    self.pre_processor.pad_token_id,
+                    eos_token,
+                )
+            )
+            input_tokens = input_tokens[split_index:]
+            input_tokens[0] = bos_token
+
+        # Add last chunk
+        chunks.append(
+            end_pad_tokens(
+                input_tokens[:split_index],
+                self.max_tokens_length,
+                self.pre_processor.pad_token_id,
+                eos_token,
+            )
+        )
+        return torch.tensor(chunks)
+
+    def forward(self, inputs):
+        input_tokens = inputs["input_ids"][0].tolist()
+        bos_token = input_tokens[0]
+        eos_token = input_tokens[-1]
+
+        input_ids = self.split_chunks(input_tokens, bos_token, eos_token)
+
+        return self.model.generate(
+            input_ids, forced_bos_token_id=inputs["forced_bos_token_id"]
+        )
+
+    def decode(self, outputs):
+        decoded_batch = self.post_processor.batch_decode(
+            outputs, skip_special_tokens=True
+        )
+        return " ".join(decoded_batch)
